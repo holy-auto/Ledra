662a23772c810ee203c501cdc27644089e429a87
HOLY AUTO <info@holy-auto.com>
2026-03-04 21:29:53 +0900

    admin: gate all pages to billing when is_active=false

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,13 @@
+import BillingGate from "./BillingGate";
 import BillingFetchGuard from "./BillingFetchGuard";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <BillingFetchGuard />
+      <BillingGate />
       {children}
     </>
   );
 }
+
